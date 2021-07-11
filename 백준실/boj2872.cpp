e59500c8f84475e725a47aeb199d9bbca48f051c
Jong Min Lee <chanmin4@naver.com>
2021-07-12 00:20:23 +0900

    boj2872

---
+++ 백준실/boj2872.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<stdio.h>
+#include<cstring>
+#include<cstdio>
+#include<cstring>
+#include<set>
+#include<map>
+#include<list>
+#include<unordered_set>
+#include<time.h>
+#define fup(i,a,b,c) for(int i=a;i<=b;i+=c)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef long long int lli;
+typedef pair<ll, ll>P;
+typedef pair<ld, ld>dP;
+ll MOD = 1e6;
+ll n, m, t, l, c;
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    cin >> n;
+    vector<ll>a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    ll temp = n;
+    for (int i = n - 1; i >= 0; i--) {
+        if (a[i] == temp)temp--;
+    }
+    cout << temp;
+    
+}
