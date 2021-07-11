c629ac5925477b24fbb000cae9cf55213c2f1d8f
Jong Min Lee <chanmin4@naver.com>
2021-07-12 00:57:12 +0900

    boj1806
    투포인터의 기본적인형태는 자연수일때만 성립함을 기억해야함

---
+++ 백준골/boj1806.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<stdio.h>
+#include<cstring>
+#include<cstdio>
+#include<cstring>
+#include<set>
+#include<map>
+#include<list>
+#include<unordered_set>
+#include<time.h>
+#define fup(i,a,b,c) for(int i=a;i<=b;i+=c)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef long long int lli;
+typedef pair<ll, ll>P;
+typedef pair<ld, ld>dP;
+ll MOD = 1e6;
+ll n, m, t, l, c;
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    cin >> n >> m;
+    vector<ll>a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    ll mini = 1e10, sum = 0, l = 0, r = 0;
+    while (1) {
+        if (sum >= m) {
+            mini = min(mini, r - l);
+            sum -= a[l++];
+        }
+        else if (r == n)break;
+        else sum += a[r++];
+    }
+    if (mini == 1e10)cout << 0;
+    else cout << mini;
+}
