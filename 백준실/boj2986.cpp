57ab613c0a1d4a4ef7126865b64b9ed92243069a
Jong Min Lee <chanmin4@naver.com>
2021-07-12 00:06:25 +0900

    boj2986
    prime number

---
+++ 백준실/boj2986.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<stdio.h>
+#include<cstring>
+#include<cstdio>
+#include<cstring>
+#include<set>
+#include<map>
+#include<list>
+#include<unordered_set>
+#include<time.h>
+#define fup(i,a,b,c) for(int i=a;i<=b;i+=c)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef long long int lli;
+typedef pair<ll, ll>P;
+typedef pair<ld, ld>dP;
+ll MOD = 1e6;
+ll n, m, t, l, c;
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    cin >> n;
+    ll cnt = 0;
+    ll num = 1;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            num = n / i;
+            break;
+        }
+    }
+    cout << n - num;
+}
