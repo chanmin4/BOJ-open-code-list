8136308c221aab2e1289b2f3c9c9c9e983ffdd59
Jong Min Lee <chanmin4@naver.com>
2021-08-23 00:27:33 +0900

    boj1965
    dp

---
+++ 20210816-20210822/boj1965.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<stdio.h>
+#include<cstring>
+#include<cstdio>
+#include<cstring>
+#include<set>
+#include<map>
+#include<list>
+#include<unordered_set>
+#include<time.h>
+#define fup(i,a,b,c) for(int i=a;i<=b;i+=c)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef long long int lli;
+typedef pair<ll, ll>P;
+typedef pair<ld, ld>dP;
+ll MOD = 1e6;
+ll n, t, m, k;
+vector<ll>arr;
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	cin >> n;
+	arr.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	ll sol = 0;
+	vector<ll>dp(n, 1);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		sol = max(sol, dp[i]);
+	}
+	cout << sol;
+}
