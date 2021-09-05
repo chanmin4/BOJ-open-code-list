43c4b3e194f54861b49ad2ad2657e3002bdcfb50
Jong Min Lee <chanmin4@naver.com>
2021-09-05 21:43:27 +0900

    boj9659

---
+++ 백준실/implementation/boj9659.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<stdio.h>
+#include<cstring>
+#include<cstdio>
+#include<cstring>
+#include<set>
+#include<map>
+#include<list>
+#include<unordered_set>
+#include<time.h>
+#define fup(i,a,b,c) for(int i=a;i<=b;i+=c)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef long long int lli;
+typedef pair<ll, ll>P;
+typedef pair<ld, ld>dP;
+ll MOD = 1e6;
+ll n, t, m, k, test;
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    cin >> n;
+    ll result = n / 3 + n % 3;
+    if (result % 2 == 1)cout << "SK";
+    if (result % 2 == 0)cout << "CY";
+
+}
+
